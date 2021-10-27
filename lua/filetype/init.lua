f96115b4b8f156962c01a39efd26b163b6ce4557
Nathan Thomas <nathanthomas707@gmail.com>
2021-10-27 15:25:34 -0700

    Fix issue where overrides would fail

--- lua/filetype/init.lua
+++ lua/filetype/init.lua
@@ -1,5 +1,6 @@
 -- generate the filetype
-local map = require("filetype.mappings")
+local builtin_map = require("filetype.mappings")
+local custom_map = nil
 
 -- Lua implementation of the setfiletype builtin function.
 -- See :help setf
@@ -69,28 +70,13 @@ local M = {}
 
 function M.setup(opts)
     if opts.overrides then
-        for table_name, table in pairs(opts.overrides) do
-            for filename, filetype in pairs(table) do
-                map[table_name][filename] = filetype
-            end
-        end
+        custom_map = opts.overrides
     end
 end
 
-function M.resolve()
-    -- Just in case
-    vim.g.did_load_filetypes = 1
-
-    local filetype
-
-    local filename = vim.fn.expand("%:t")
-    if filename == "" then
-        return
-    end
-
-    -- Extension
-    local ext = filename:match("%.(%w+)$")
-
+-- Trys to match the tables in map to the filename components given
+-- Returns true if a match was found, otherwise false
+local function try_filetype_map(absolute_path, filename, ext, map)
     -- We first check the ones that only require a table lookup
     -- because they're the fastest
 
@@ -99,7 +85,7 @@ function M.resolve()
         filetype = map.extensions[ext] or map.function_extensions[ext]
         if filetype then
             set_filetype(filetype)
-            return
+            return true
         end
     end
 
@@ -107,28 +93,67 @@ function M.resolve()
     local literal = map.literal[filename] or map.function_literal[filename]
     if literal then
         set_filetype(literal)
+        return true
     end
 
     -- Finally, we check the ones that require regexes.
     -- try_regex is relatively slow because it has to iterate through
     -- the key-value pairs and run a regex for each one
 
-    local absolute_path = vim.fn.expand("%:p")
-
-    -- The endswith table is left separate in case there an an optimization t
+    -- The endswith table is left separate in case there an an optimization
     -- that can be applied later. As of now, it's just using regexes.
     if try_regex(absolute_path, map.endswith) then
-        return
+        return true
     end
 
     if try_regex(absolute_path, map.complex) then
-        return
+        return true
     end
 
     -- These require the use of a special function that excludes
     -- certain filetypes from being binded to autocommands
     -- using g:ft_ignore_pat
     if try_regex(absolute_path, map.star_sets, true) then
+        return true
+    end
+
+    return false
+end
+
+function M.resolve()
+    -- Just in case
+    vim.g.did_load_filetypes = 1
+
+    local filetype
+
+    local expand = vim.fn.expand
+
+    local filename = expand("%:t")
+    if filename == "" then
+        return
+    end
+
+    -- Extension
+    local ext = filename:match("%S%.(%w+)$")
+
+    local absolute_path = expand("%:p")
+
+    -- Try to match the custom defined filetypes
+    if custom_map ~= nil then
+        -- Avoid indexing nil
+        for table_name, _ in pairs(builtin_map) do
+            if custom_map[table_name] == nil then
+                custom_map[table_name] = {}
+            end
+        end
+
+        if try_filetype_map(absolute_path, filename, ext, custom_map) then
+            return
+        end
+    end
+
+    -- Try to match filename to builtin filetypes
+    if try_filetype_map(absolute_path, filename, ext, builtin_map) then
         return
     end
 
@@ -143,7 +168,7 @@ function M.resolve()
     -- and set the filetype to that
     local shebang = analyze_shebang()
     if shebang then
-        shebang = map.shebang[shebang] or shebang
+        shebang = builtin_map.shebang[shebang] or shebang
         set_filetype(shebang)
     end
 end
